bb0524f85e6c97ab1bcd2341c1b38cb14aa4053a
emanuel <emamanriquez1991@gmail.com>
2023-04-03 23:16:10 -0300

    2do commit

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -7,6 +7,58 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: "The field can't be null"
+        }
+      }
     },
+    id: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    min_height:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "The field can't be null"
+        }
+      }
+    },
+    max_height:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "The field can't be null"
+        }
+      }
+    },
+    min__weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "The field can't be null",
+        },
+      },
+    },
+    max__weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "The field can't be null",
+        },
+      },
+    },
+    life_spam:{
+      type: DataTypes.STRING,
+    }
   });
 };
