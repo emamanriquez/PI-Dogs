82311065775aaab114f0cb6fab830cf79969d7c9
emamanriquez <emamanriquez1991@gmail.com>
2023-06-25 19:56:37 -0300

    nuevos cambios deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,PGPASSWORD } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dog`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dog`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+const sequelize = new Sequelize(
+  PGPASSWORD,
+  {logging: false,
+  native: false
+}
+
+
+
+)
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
